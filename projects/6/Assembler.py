16d57a0d300c90dbd0852e74f8c24267f0a8ab7b
shiba <2255070t@gsuite.kobe-u.ac.jp>
2025-06-09 17:28:59 +0900

    fix 基本版アセンブラ

--- projects/6/Assembler.py
+++ projects/6/Assembler.py
@@ -6,13 +6,13 @@ C_INSTRUCTION = 1
 L_INSTRUCTION = 2
 
 class Assembler:
-    def __init__(self, filename):
-        self.parser = Parser(filename)
+    def __init__(self, source_filepath):
+        self.parser = Parser(source_filepath)
         self.code = Code()
         self.binary_code = ""
 
     
-    def assemble(self):
+    def assemble(self, target_filepath):
         while self.parser.hasMoreLines():
             self.parser.advance()
             binary_instruction  = ""
@@ -21,4 +21,10 @@ class Assembler:
             elif (self.parser.instructionType() == C_INSTRUCTION):
                 binary_instruction = "111" + self.code.comp(self.parser.comp()) + self.code.dest(self.parser.dest()) + self.code.jump(self.parser.jump())
             self.binary_code += binary_instruction + "\n"
+        try:
+            with open(target_filepath, 'w') as file:
+                file.write(self.binary_code)
+                print("success!")
+        except IOError as e:
+            print(f"ファイルの書き込み中にエラーが発生しました: {e}")
 
--- projects/6/main.py
+++ projects/6/main.py
@@ -4,5 +4,5 @@ from Assembler import Assembler
 
 
 assembler = Assembler("/Users/hiroki/Desktop/app/myapp/nand2tetris/projects/6/add/Add.asm")
-assembler.assemble()
+assembler.assemble("/Users/hiroki/Desktop/app/myapp/nand2tetris/projects/6/add/Add.hack")
 print(assembler.binary_code)
